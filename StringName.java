d384a43a83b47225ba62529f64f0718b345ed9ab
aaron_tetracube <aaronpsam@gmail.com>
2021-04-06 23:37:27 +0530

    added.

--- StringName.java
+++ StringName.java
@@ -6,7 +6,8 @@ public class StringName {
        Scanner sc = new Scanner(System.in);
        System.out.println("Enter a String");
        name = sc.nextLine();
-       sc.close();
        System.out.println(name);
+       
+
    } 
 }
