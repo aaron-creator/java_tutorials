0be9ebd619dc660341ad81aacb3cd22772673b1a
aaron_tetracube <aaronpsam@gmail.com>
2021-07-01 19:45:12 +0530

    added

--- StringName.java
+++ StringName.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 public class StringName {
    public static void main(String[] args) {
        String name;
-       Scanner sc = new Scanner(System.in);
+       Scanner sc = new Scanner(System.in);ra
        System.out.println("Enter a String");
        name = sc.nextLine();
        System.out.println(name);
